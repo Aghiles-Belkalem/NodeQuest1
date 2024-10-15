62e13be2a0ebd27d8e91059053b843243dffd319
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2024-10-15 11:26:25 +0200

    quest nodeJs

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Hello nico!'));
+console.log(chalk.blue('Hello pierre!'));
+console.log(chalk.red('Hello marwane!'));
+console.log(chalk.yellow('Hello thomas!'));
